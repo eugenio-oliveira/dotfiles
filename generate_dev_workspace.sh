fa2b144f3d5939ff66c7686c3d58138cf013b708
Eugênio Oliveira <eugenio.p.oliveira@outlook.com>
2018-09-23 15:35:02 -0300

    add script description

--- generate_dev_workspace.sh
+++ generate_dev_workspace.sh
@@ -1,4 +1,8 @@
-# 
+# this script generates the default workspace for web and mobile development
+# author: Eugênio Oliveira
+# site: https://eugeniopaccelli.github.io/profile/
+# creation date: 23/09/2018
+
 echo "Hello, this file will generate your development workspace..."
 sleep 1
 
@@ -21,7 +25,7 @@ while true; do
     case $yn in
         [Yy]* ) echo "Ok, Let's continue..."; generate_folders; break;;
         [Nn]* ) echo "Installation aborted.";exit;;
-        * ) echo "Please answer Yes (Yy) or No (Nn).";;
+        * ) echo "Please, answer Yes (Yy) or No (Nn).";;
     esac
 done
 
